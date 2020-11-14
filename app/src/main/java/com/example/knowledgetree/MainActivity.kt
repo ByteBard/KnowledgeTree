19676bc0f6da69ae4941e2bfdcc0f4450b2aef14
ByteBard <liumingvic@gmail.com>
2020-11-15 02:43:39 +1100

    add navigation

--- app/src/main/java/com/example/knowledgetree/MainActivity.kt
+++ app/src/main/java/com/example/knowledgetree/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.database.*
 import java.util.*
 
@@ -13,44 +15,70 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val loginBtn = findViewById<Button>(R.id.login_button)
-
-        loginBtn.setOnClickListener {
-            val intent = Intent(this, FilterActivity::class.java)
-            val currentTime = Calendar.getInstance().time
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.selectedItemId = R.id.navigation_home
 
 
-            var ref = FirebaseDatabase.getInstance().getReference("tasks")
-            val taskList1 = mutableListOf<Task>()
 
-            val queryRef: Query = ref.orderByChild("taskId").equalTo("-MLmM8C6NPh1fK2sTkTm")
-
-
-            queryRef.addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    taskList1.clear()
-                    for (postSnapshot in dataSnapshot.children) {
-                        val task: Task? =
-                            postSnapshot.getValue(Task::class.java)
-                        if (task != null) {
-                            taskList1.add(task)
-                        }
-                    }
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_issues -> {
+                    val intent = Intent(this, IssueListActivity::class.java)
+                    intent.putExtra("filter", "filter")
+                    startActivityForResult(intent, 1)
                 }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-                    println("The read failed: " + databaseError.code)
-                }
-            })
-
-
-
-
-
-
-
-            intent.putExtra("login", "login")
-            startActivityForResult(intent, 0)
+//                R.id.action_schedules -> {
+//                }
+//                R.id.action_music -> {
+//                }
+            }
+            true
         }
+
+
+
+
+
+
+
+//        val loginBtn = findViewById<Button>(R.id.login_button)
+//
+//        loginBtn.setOnClickListener {
+//            val intent = Intent(this, FilterActivity::class.java)
+//            val currentTime = Calendar.getInstance().time
+//
+//
+//            var ref = FirebaseDatabase.getInstance().getReference("tasks")
+//            val taskList1 = mutableListOf<Task>()
+//
+//            val queryRef: Query = ref.orderByChild("taskId").equalTo("-MLmM8C6NPh1fK2sTkTm")
+//
+//
+//            queryRef.addValueEventListener(object : ValueEventListener {
+//                override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                    taskList1.clear()
+//                    for (postSnapshot in dataSnapshot.children) {
+//                        val task: Task? =
+//                            postSnapshot.getValue(Task::class.java)
+//                        if (task != null) {
+//                            taskList1.add(task)
+//                        }
+//                    }
+//                }
+//
+//                override fun onCancelled(databaseError: DatabaseError) {
+//                    println("The read failed: " + databaseError.code)
+//                }
+//            })
+//
+//
+//
+//
+//
+//
+//
+//            intent.putExtra("login", "login")
+//            startActivityForResult(intent, 0)
+//        }
     }
 }
