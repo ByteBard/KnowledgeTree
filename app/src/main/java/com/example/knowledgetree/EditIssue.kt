1d4b974e0acc1d9a5008f13754375547fcc440df
ByteBard <liumingvic@gmail.com>
2020-11-16 08:49:13 +1100

    validate issue update

--- app/src/main/java/com/example/knowledgetree/EditIssue.kt
+++ app/src/main/java/com/example/knowledgetree/EditIssue.kt
@@ -3,8 +3,11 @@ package com.example.knowledgetree
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.database.*
 import java.util.*
 
@@ -16,6 +19,7 @@ class EditIssue : AppCompatActivity() {
     private var issueRef: DatabaseReference = FirebaseDatabase.getInstance().getReference("issues")
     private var issueUpdateListener: ValueEventListener? = null
     private var removedMsg = "Issue Successfully Removed!"
+    private var isValid = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,45 +72,67 @@ class EditIssue : AppCompatActivity() {
         }
 
         updateIssueButton.setOnClickListener {
-            val title = findViewById<EditText>(R.id.issue_create_title_input_text).text.toString()
-            val type = findViewById<EditText>(R.id.issue_create_type_input_text).text.toString()
-            val detail = findViewById<EditText>(R.id.issue_create_detail_input_text).text.toString()
-            val complete = findViewById<CheckBox>(R.id.issue_create_complete_checkbox).isChecked
-            val currentTime: Date = Calendar.getInstance().time
-            issue?.let {
-                val updatedIssue = Issue(
-                    it.issueId,
-                    title,
-                    detail,
-                    type,
-                    it.progress,
-                    complete,
-                    it.createdTimestamp,
-                    currentTime
-                )
+            validateInput()
+            if(isValid){
+                val title = findViewById<EditText>(R.id.issue_create_title_input_text).text.toString()
+                val type = findViewById<EditText>(R.id.issue_create_type_input_text).text.toString()
+                val detail = findViewById<EditText>(R.id.issue_create_detail_input_text).text.toString()
+                val complete = findViewById<CheckBox>(R.id.issue_create_complete_checkbox).isChecked
+                val currentTime: Date = Calendar.getInstance().time
+                issue?.let {
+                    val updatedIssue = Issue(
+                        it.issueId,
+                        title,
+                        detail,
+                        type,
+                        it.progress,
+                        complete,
+                        it.createdTimestamp,
+                        currentTime
+                    )
 
-                issueRef.child(it.issueId).setValue(updatedIssue)
-                    .addOnSuccessListener {
-                        Toast.makeText(
-                            applicationContext,
-                            issueUpdatedMsg,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                    .addOnFailureListener {
-                        Toast.makeText(
-                            applicationContext,
-                            issueUpdateFailureMsg,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                    issueRef.child(it.issueId).setValue(updatedIssue)
+                        .addOnSuccessListener {
+                            Toast.makeText(
+                                applicationContext,
+                                issueUpdatedMsg,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(
+                                applicationContext,
+                                issueUpdateFailureMsg,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                }
+                Toast.makeText(
+                    applicationContext,
+                    removedMsg,
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
             }
-            Toast.makeText(
-                applicationContext,
-                removedMsg,
-                Toast.LENGTH_SHORT
-            ).show()
-            finish()
+        }
+    }
+
+    private fun validateInput(){
+        val titleEditView = findViewById<EditText>(R.id.issue_create_title_input_text)
+        val typeEditView = findViewById<EditText>(R.id.issue_create_type_input_text)
+        val detailView = findViewById<EditText>(R.id.issue_create_detail_input_text)
+
+        if(titleEditView.text.isNullOrEmpty()){
+            titleEditView.error = "Title can not be empty!"
+            isValid = false
+        }else if(typeEditView.text.isNullOrEmpty()){
+            typeEditView.error = "Type can not be empty!"
+            isValid = false
+        }else if(detailView.text.isNullOrEmpty()){
+            detailView.error = "Detail can not be empty!"
+            isValid = false
+        }else{
+            isValid = true
         }
     }
 
